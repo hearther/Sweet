c95313c5d6a9ec8c379d6ea3180a3b58e00a5ad9
hearther <hearther@users.noreply.github.com>
2024-09-18 19:24:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ var package = Package(
       targets: ["Sweet"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/hearther/HTTPClient", branch: "main"),
+    .package(url: "https://github.com/hearther/HTTPClient", branch: "master"),
     .package(url: "https://github.com/apple/swift-format", branch: "main"),
     .package(url: "https://github.com/zunda-pixel/OAuth1", .upToNextMajor(from: "1.0.0"))
   ],
